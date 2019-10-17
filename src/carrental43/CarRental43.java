20d8d4120d4d309a33a1678e9d08a07ce0df9331
Thor <thor@10.14.6>
2019-10-17 12:20:32 +0200

    first commit

---
+++ src/carrental43/CarRental43.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrental43;
+
+/**
+ *
+ * @author thor
+ */
+public class CarRental43 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
