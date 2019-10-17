d1471cedff9fd61bca9b6b08744cb862d92638b5
Thor <thor@10.14.6>
2019-10-17 15:39:20 +0200

    second task

---
+++ test/carrental43/CustomerTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrental43;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author thor
+ */
+public class CustomerTest {
+	Customer myCustomer;
+	
+	@Before
+	public void setUp() {
+		myCustomer = new Customer("Anton Verner","Individual","test@verner.dk","DK","Vildgade 12","5000 Odense C");
+	}
+
+	@Test
+	public void testgetStreet() {
+		String expected = "Vildgade 12";
+		String actual = myCustomer.getStreet();
+		assertEquals(expected, actual);
+	}
+	
+}
