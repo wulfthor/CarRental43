4799001c79f035e40a8cb5b3c562b2ebc89f52fb
Thor <thor@10.14.6>
2019-10-17 15:51:18 +0200

    second solution

--- src/carrental43/Customer.java
+++ src/carrental43/Customer.java
@@ -10,5 +10,25 @@ package carrental43;
  * @author thor
  */
 public class Customer {
-	
+
+	String name;
+	String custType;
+	String email;
+	String Country;
+	String street;
+	String city;
+
+	public Customer(String name, String custType, String email, String Country, String street, String city) {
+		this.name = name;
+		this.custType = custType;
+		this.email = email;
+		this.Country = Country;
+		this.street = street;
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return this.street;
+	}
+
 }
