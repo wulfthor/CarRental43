abba0117eed790dc1630e8c89ed96523bc66e044
Thor <thor@10.14.6>
2019-10-18 12:46:48 +0200

    fifth task

--- test/carrental43/EmployeeTest.java
+++ test/carrental43/EmployeeTest.java
@@ -1,6 +1,6 @@
 package carrental43;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -10,23 +10,30 @@ import static org.junit.Assert.*;
  * @author thor
  */
 public class EmployeeTest {
+	String filename;
 	Employee admin;
 	Garage garage;
 	
 	@Before
 	public void setUp() {
+		filename = "Data/cars.csv";
 		admin = new Employee("Mette","Admin");
 		garage = new Garage("Gentofte");
 		garage.setAddress("Gentofte","2820", "Lindevej", "12");
 	}
-
+	
 	@Test
-	public void testfillGarageFromFile(String filename) {
-		admin.fillGarageFromFile(filename);
-		int expectedsize = 5;
-		int actualsize = orderDetails.size();
-		assertEquals(expectedsize, actualsize);
-
+	public void testImportCarsFromFilename() throws Exception {
+		System.out.println("importCarsFromFilename");
+		ArrayList<Car> myCar = admin.importCarsFromFilename(filename);
+		for (Car car : myCar) {
+			garage.addCar(car);
+		}
+		String retVal = garage.toString();
+		System.out.print(retVal);
+		int actual = myCar.size();
+		int expected = 8;
+		assertEquals(expected, actual);
 	}
 	
 }
