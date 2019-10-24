7075caa0acc8dca93b0bcef269c7074266107971
Thor <thor@10.14.6>
2019-10-24 14:17:53 +0200

    car

--- test/carrental43/CarTest.java
+++ test/carrental43/CarTest.java
@@ -11,16 +11,13 @@ import static org.junit.Assert.*;
 public class CarTest {
 	Car myCar;
 	
-	public CarTest() {
-	}
-	
 	@Before
 	public void setUp() {
 		// String make, String model, String type, int doors, int cyl, int HK, int MPG,int weight, int length, int year, int milage
 		//Obs;Make;Model;Cylinders;Horsepower;MPG_City;MPG_Highway;Weight;Wheelbase;Year;Milage
 		//1;Acura;MDX;6;265;17;23;4451;106;1997;19
 		//2;Acura;RSX Type S 2dr;4;200;24;31;2778;101;1998;12000
-		myCar = new Car("Acura","MDX","S",4,6,265,17,4451,106,1997,19);
+		myCar = new Car("Acura","RSX","S",4,6,265,17,4451,106,1997,19);
 	}
 
 	@Test
